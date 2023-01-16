51a58148a993d9d0c16a1783fd719b4ee8722995
sultan <sultan_33_886@outlook.sa>
2023-01-16 13:05:11 +0300

    lab_TUPLES

--- tuple lab.py
+++ tuple lab.py
@@ -2,7 +2,9 @@ number_val=(1,2,3)
 word_val=("a","b","c")
 result=number_val+word_val
 ls=[4,5,6]
-var1,var2,var3,var4,var5,var6=result
+var1,var2,var3=tuple(ls)
+find=word_val.index("b")
+f=word_val.count(2)
 print(number_val)
 print(word_val)
 print(result)
@@ -11,9 +13,7 @@ print(tuple(ls))
 print(var1)
 print(var2)
 print(var3)
-print(var4)
-print(var5)
-print(var6)
+print(find)
 
  
 
