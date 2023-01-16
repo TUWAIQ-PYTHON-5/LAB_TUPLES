13092dc396fcdfbc786479179d3cc1fafd105c73
محمد الزيداني <zidani2030@gmail.com>
2023-01-16 11:50:31 +0300

    solved lab

---
+++ lab.py
@@ -0,0 +1,21 @@
+numbers = (1,2,3)
+letters = ("a","b","c")
+print(numbers[0])
+print(letters[2])
+
+result=numbers+letters
+print(result)
+
+if 4 in result:
+    print("Number 4 is available")
+else:
+    print("Number 4 is not available")
+
+
+num = [4,5,6]
+print(tuple(num))
+
+
+x , y , z = num
+
+print(x,y,z)
