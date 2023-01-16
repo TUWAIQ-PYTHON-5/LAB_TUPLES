06e583d55564e521f112d18eb0595d38250adc24
sultan <sultan_33_886@outlook.sa>
2023-01-16 11:50:10 +0300

    lab_TUPLES

---
+++ tuple lab.py
@@ -0,0 +1,19 @@
+number_val=(1,2,3)
+word_val=("a","b","c")
+result=number_val+word_val
+ls=[4,5,6]
+var1,var2,var3,var4,var5,var6=result
+print(number_val)
+print(word_val)
+print(result)
+print(result in[4])
+print(tuple(ls))
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+
+ 
+
