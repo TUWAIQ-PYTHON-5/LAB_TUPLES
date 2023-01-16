d830abdbe09b9c9e84d2d192f45360f61ffc1134
unknown <dalmq4806@gmail.comgit config --global user .name dalalgit config --global user.email dalmq4806@gmail.com>
2023-01-16 12:10:44 +0300

    lap.10

---
+++ tuple.py
@@ -0,0 +1,23 @@
+numbers = 1,2,3
+letters = "a","b","c"
+print(numbers[0])
+print((letters[-1]))
+
+result =(numbers+letters)
+print(result)
+print(len(result))
+
+res = 4 in result
+print(res)
+
+
+def convert (list):
+    return tuple(list)
+list = ([4,5,6])
+N = convert(list)
+print(N)
+
+
+
+let_1,let2,let3 = N
+print(let_1,let2,let3)
