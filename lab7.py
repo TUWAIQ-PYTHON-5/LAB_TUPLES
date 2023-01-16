04789b319d5e6390ba7f5ba897a662155d47304c
atheer <atheeralq0@gmail.com>
2023-01-16 13:31:15 +0300

    end

--- lab7.py
+++ lab7.py
@@ -1,8 +1,8 @@
-numbers =(1,2,3)
+numbers_tuple =(1,2,3)
 letters = ("a","b","c")
-print("first number: ",numbers[0])
-print("first letter: ",letters[0])
-result=(numbers+letters)
+print("first number: ",numbers_tuple[0])
+print("first letter: ",letters[-1])
+result=(numbers_tuple+letters)
 print("length for both tuples: ",len(result))
 x=input("search for a number:  ")
 if x in result:
@@ -18,7 +18,8 @@ def convert (the_list :list)-> tuple:
 
 number_list=[4,5,6]
 print(convert(number_list))
-var,var1,var3=numbers
+var,var1,var3=numbers_tuple
+print(var,var1,var3)
 print("find the index of the string b: ",letters.index("b"))
 print("find the number of occurrences of the integer 2: ",result.count(2))
 for index,value in enumerate(result):
